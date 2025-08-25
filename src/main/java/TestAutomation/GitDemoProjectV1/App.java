eee0a8b935139b7395e9ff51ad8554dea13a8e76
A184112 <A184112@DESKTOP-H7BOP2P>
2025-08-25 18:19:16 +0530

    first change

---
+++ src/main/java/TestAutomation/GitDemoProjectV1/App.java
@@ -0,0 +1,10 @@
+package TestAutomation.GitDemoProjectV1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
